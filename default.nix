cb01e17015326920abf113baa5ba69a736b84b7b
crolbar <crolbar@crolbar.com>
2024-03-09 21:26:46 +0200

    t

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {};
 rustPlatform.buildRustPackage rec {
-  pname = "hello";
+  pname = "fnix";
   version = "0.1";
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
