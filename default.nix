331b31f76b870d5503cad0e30386f31be6e815b8
crolbar <crolbar@crolbar.com>
2024-03-10 12:57:24 +0200

    t

--- default.nix
+++
@@ -1,7 +0,0 @@
-with import <nixpkgs> {};
-rustPlatform.buildRustPackage rec {
-  pname = "testnix";
-  version = "0.1";
-  cargoLock.lockFile = ./Cargo.lock;
-  src = pkgs.lib.cleanSource ./.;
-}
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+            inherit system overlays;
+        };
+       rustVersion = pkgs.rust-bin.stable.latest.default;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        test = rustPlatform.buildRustPackage {
+            pname = "testnix";
+            version = "0.1";
+            cargoLock.lockFile = ./Cargo.lock;
+            src = ./.;
+        };
+
+      in with pkgs;
+      {
+        defaultPackage = test;
+        devShells.default = mkShell {
+          buildInputs = [
+            pkg-config
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+          ];
+        };
+      }
+    );
+}
